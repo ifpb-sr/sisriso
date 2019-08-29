5d811df71f1f6e035cf64740a616132f42eb38c9
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-08-29 00:46:20 +0000

    Criação de tabelas #21

--- app.py
+++ app.py
@@ -11,25 +11,58 @@ from flask_migrate import Migrate, upgrade
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-if os.environ.get('DYNO'):
+'''if os.environ.get('DYNO'):
     # Produção no heroku
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 else:
     app.config['SECRET_KEY'] = 'hard to guess string'
     basedir = os.path.abspath(os.path.dirname(__file__))
     app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')'''
 
 
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+  
+'''class Dentista(db.Model):
+    __tablename__ = 'dentistas'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(128))
+    
+    
+    @staticmethod
+    def inserir_tipos():
+        db.session.add(Dentista(nome="Ismael"))
+        db.session.add(Dentista(nome="Alves"))
+        db.session.add(Dentista(nome="Lima"))
+        db.session.commit()'''
+
+class Pacientes(db.Model):
+    __tablename__ = 'PACIENTES'
+    id = db.Column(db.String(4), primary_key=True)
+    cpf = db.Column(db.String(14))
+    nome = db.Column(db.String(64))
+    tipoSanguinio = db.Column(db.String(3))
+    idade = db.Column(db.Integer)
+    sexo = db.Column(db.String(1))
+    # endereco
+    rua = db.Column(db.String(64))
+    bairo = db.Column(db.String(64))
+    cep = db.Column(db.String(9))
+    numero = db.Column(db.String(5))
+    cidade = db.Column(db.String(34))
+    estado = db.Column(db.String(34))
+    # fim endereco
+    rg = db.Column(db.String(14))
+
 @app.route('/')
 def inicio():
     return render_template("base.html")
+
 
 @app.route('/clientes')
 def clientes():
@@ -51,7 +84,7 @@ def cadastroSecretaria():
     return render_template('secretaria/cadastroSecretaria.html')
 
 
-@app.shell_context_processor
+'''@app.shell_context_processor
 def make_shell_context():
     return dict(db=db, Dentista=Dentista)
 
@@ -59,4 +92,4 @@ def make_shell_context():
 def deploy():
     """Run deployment tasks."""
     upgrade()
-    Dentista.inserir_tipos()
+    Dentista.inserir_tipos()'''
