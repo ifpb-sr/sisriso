7110c2f50bd347d1c6f7a97f89709326b49e93c8
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-09-01 00:40:29 +0000

    O retorno do SQLite #37

--- app.py
+++ app.py
@@ -8,11 +8,15 @@ from wtforms.widgets.html5 import NumberInput
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, upgrade
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
