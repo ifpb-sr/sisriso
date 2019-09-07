dabd74ea0b72c69bd3d9a165ea2319d2b5b4e50a
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-09-07 14:40:29 -0300

    Correções nas tabelas do banco

--- app.py
+++ app.py
@@ -22,17 +22,20 @@ bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-usam = db.Table("usam",
-    db.Column('paciente_id', db.Integer, db.ForeignKey('PACIENTES.id')),
-    db.Column('medico_id', db.Integer, db.ForeignKey('MEDICOS.id'))
-) #REVISADO
+tomar = db.table('tomar',
+        db.Column('paciente_id', db.Integer, db.ForeignKey('PACIENTES.id')),
+        db.Column('medicamento_id', db.Integer,db.ForeignKey('MEDICAMENTOS.id'))
+)
+possuir = db.table('possuir',
+        db.Column('paciente_id', db.Integer, db.ForeignKey('PACIENTES.id')),
+        db.Column('doenca_id', db.Integer, db.ForeignKey('DOENCAS.id')),
+)
+receitar = db.table('receitar',
+        db.Column('medicamento_id', db.Integer, db.ForeignKey('MEDICAMENTOS.id')),
+        db.Column('medico_id', db.Integer, db.ForeignKey('MEDICOS.id')),
+)
 
-possuem = db.Table("possuem",
-    db.Column('paciente_id', db.Integer, db.ForeignKey('PACIENTES.id')),
-    db.Column('doenca_id', db.Integer, db.ForeignKey('DOENCAS.id'))
-) #REVISADO
-
-class Pacientes(db.Model): #REVISADO
+class Pacientes(db.Model):
     __tablename__ = 'PACIENTES'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(64))
@@ -40,111 +43,80 @@ class Pacientes(db.Model): #REVISADO
     idade = db.Column(db.Integer)
     sexo = db.Column(db.String(1))
     rg = db.Column(db.String(14))
-    cpf = db.Column(db.String(14), unique=True)
+    cpf = db.Column(db.String(14))
     profissao = db.Column(db.String(64))
     indicacao = db.Column(db.String(64))
-    # endereco
+    ###ENDERECO###
     rua = db.Column(db.String(64))
-    bairo = db.Column(db.String(64))
+    bairro = db.Column(db.String(64))
     cep = db.Column(db.String(9))
     numero = db.Column(db.String(5))
-    cidade = db.Column(db.String(34))
-    estado = db.Column(db.String(34))
-    # fim endereco
-    #urgencia
+    cidade = db.Column(db.String(32))
+    estado = db.Column(db.String(2))
+    ###URGENCIA###
     urgenciaNome = db.Column(db.String(64))
     urgenciaTelefone = db.Column(db.String(11))
     observacao = db.Column(db.String(128))
-    #fim urgencia
-    #MATR convenio
+    ###MATRICULA Convenio###
     matr = db.Column(db.String(14))
-    #-------------------------------
-    
-    contatos_id = db.Column(db.Integer, db.ForeignKey('CONTATOS.id')) #1-N REVISADO
-    convenios = db.relationship('Convenios', backref='PACIENTES') #REVISADO
-    
-    doencas = db.relationship('Doencas', backref='PACIENTES')
-    anamneses = db.relationship('Anamneses', uselist=False, backref='PACIENTES')  #REVISADO
-    
+    ###CHAVES ESTRANGEIRAS###
+    convenio_id = db.Column(db.Integer, db.ForeignKey('CONVENIOS.id'), default=0) #PK
+    ###RELACIONAMENTOS###
+    anamnese = db.relationship('Anamneses', backref='PACIENTES', uselist=False)
+    contatos = db.relationship('Contatos', backref='PACIENTES')
     medicamentos = db.relationship('Medicamentos',
-                                    secondary='usam',
-                                    backref=db.backref('PACIENTES', lazy='dynamic'),
-                                    lazy='dynamic') #REVISADO
-    
+                secondary=tomar,
+                backref=db.backref('PACIENTES', lazy='dynamic'),
+                lazy='dynamic')
     doencas = db.relationship('Doencas',
-                                    secondary='possuem',
-                                    backref=db.backref('PACIENTES', lazy='dynamic'),
-                                    lazy='dynamic') #REVISADO N-N
-    
-    '''@staticmethod
-    def inserir_tipos():
-        db.session.add(Dentista(nome="Ismael"))
-        db.session.add(Dentista(nome="Alves"))
-        db.session.add(Dentista(nome="Lima"))
-        db.session.commit()'''
-
-        
-class Convenios(db.Model): #REVISADO
-    __tablename__ = 'CONVENIOS'
-    id = db.Column(db.Integer, primary_key=True) #VERIFICAR
-    tipoPlano = db.Column(db.String(32))
-    
-    paciente_id = db.Column(db.Integer, db.ForeignKey('PACIENTES.id')) #REVISADO
-
-class Contatos(db.Model): #REVISADO
-    __tablename__ = 'CONTATOS'
-    id = db.Column(db.Integer, primary_key=True) #VERIFICAR
-    contato = db.Column(db.String(11))
-    
-    pacientes = db.relationship('Pacientes', backref='CONTATOS') #REVISADO
+                secondary=possuir,
+                backref=db.backref('PACIENTES', lazy='dynamic'),
+                lazy='dynamic')
 
 class Anamneses(db.Model):
     __tablename__ = 'ANAMNESES'
-    id = db.Column(db.Integer, primary_key=True) #VERIFICAR
-    
+    id = db.Column(db.Integer, primary_key=True)
     bemSaude = db.Column(db.Boolean)
     sobCuidado = db.Column(db.Boolean)
-    motivoCuidado = db.Column(db.String(64), default="")
+    motivoCuidado = db.Column(db.String(64))
     usaMedicamentos = db.Column(db.Boolean)
     hemorragia = db.Column(db.Boolean)
     fumante = db.Column(db.Boolean)
     tempoFuma = db.Column(db.Interval)
     cigarrosPorDia = db.Column(db.Integer)
-    
-    #--------------------RELATIONSHIPS e FKs-------------------
-    
-    paciente_id = db.Column(db.Integer, db.ForeignKey('PACIENTES.id'))
-    #medicamentos = db.relationship('Medicamentos', backref='ANAMNESES') #FK
-    #medicoResponsavel = db.relationship('Medicos', backref='ANAMNESES') #FK
+    ###Chaves Estrangeiras###
+    paciente_id = db.Column(db.Integer, db.ForeignKey('PACIENTES.id')) #PK
 
-receitas = db.Table ('receitas', 
-    db.Column('medicamentos_id', db.Integer, db.ForeignKey('MEDICAMENTOS.id')),
-    db.Column('medicos_id', db.Integer, db.ForeignKey('MEDICOS.id'))
-) #REVISADO
+class Contatos(db.Model):
+    __tablename__ = 'CONTATOS'
+    id = db.Column(db.Integer, primary_key=True)
+    telefone = db.Column(db.String(11))
+    paciente_id = db.Column(db.Integer, db.ForeignKey('PACIENTES.id')) #PK
+
+class Convenios(db.Model):
+    __tablename__ = 'CONVENIOS'
+    id = db.Column(db.Integer, primary_key=True)
+    tipoPlano = db.Column(db.String(16))
+    pacientes = db.relationship('Pacientes', backref='CONVENIOS')
+
+class Doencas(db.Model):
+    __tablename__ = 'DOENCAS'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(32))
 
 class Medicamentos(db.Model):
     __tablename__ = 'MEDICAMENTOS'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(32))
-    
-    medicos = db.relationship('Medicos', 
-                            secondary=receitas,
-                            backref=db.backref('MEDICAMENTOS', lazy='dynamic'),
-                            lazy='dynamic') #REVISADO
-    #anamneses_id = db.Column(db.Integer, db.ForeignKey('ANAMNESES.id'))
+    medicos = db.relationship('Medicos',
+                secondary=receitar,
+                backref=db.backref('MEDICAMENTOS', lazy='dynamic'),
+                lazy='dynamic')
 
 class Medicos(db.Model):
     __tablename__ = 'MEDICOS'
-    id = db.Column(db.Integer, primary_key=True) #VERIFICAR
-    
-    #telefone = db.relationship('Contatos', backref='MEDICOS') #TELEFONE DO MEDICO
-
-class Doencas(db.Model):
-    __tablename__ = 'DOENCAS'
-    id = db.Column(db.String(4), primary_key=True) #VERIFICAR
-    Doencas = db.Column(db.String(20))
-    
-    #paciente_id = db.Column(db.Integer, db.ForeignKey('PACIENTES.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(32))
 
 @app.route('/')
 def inicio():
